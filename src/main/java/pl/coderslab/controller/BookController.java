2f3651e6afc0708e7c601372943c7f4875b30b82
Slawomir Skrouba <swskrouba96@gmail.com>
2019-07-05 16:21:26 +0200

    Adding MemoryBookService

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,23 +1,38 @@
 package pl.coderslab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.model.Book;
+import pl.coderslab.service.MemoryBookService;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    private MemoryBookService mbs;
+
+    @Autowired
+    public BookController(MemoryBookService mbs) {
+        this.mbs = mbs;
+    }
+
     @RequestMapping("/hello")
     public String hello() {
         return "{hello: World}";
     }
 
     @RequestMapping("/helloBook")
-    public Book helloBook(){
-        return new Book(1L,"9788324631766","Thinking in Java",
-                "Bruce Eckel","Helion","programming");
+    public Book helloBook() {
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
+    }
+
+    @RequestMapping("/test")
+    public void test() {
+        mbs.editBookById(3, new Book(5,"9780130819338", "Java 2. Podstawy",
+                "Cay Horstmann, Gary Cornell", "Helion", "programming"));
     }
 }
 
