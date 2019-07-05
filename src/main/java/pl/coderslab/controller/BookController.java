ecee10942f896ba9986d977ab52be7c58700f469
Slawomir Skrouba <swskrouba96@gmail.com>
2019-07-05 15:28:15 +0200

    jackson converter + test

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,7 +1,10 @@
 package pl.coderslab.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.model.Book;
 
 @RestController
 @RequestMapping("/books")
@@ -9,6 +12,12 @@ public class BookController {
     @RequestMapping("/hello")
     public String hello() {
         return "{hello: World}";
+    }
+
+    @RequestMapping("/helloBook")
+    public Book helloBook(){
+        return new Book(1L,"9788324631766","Thinking in Java",
+                "Bruce Eckel","Helion","programming");
     }
 }
 
